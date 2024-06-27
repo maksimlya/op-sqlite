33eb74e0bf7846cb9a8c3708b99e1daa5c1f61aa
Maksim Windows <maksimlya@gmail.com>
2024-06-27 18:21:41 +0300

    Add paths and enable install method.

--- windows/OpEngineeringOpSqlite/OPSQLite.h
+++ windows/OpEngineeringOpSqlite/OPSQLite.h
@@ -4,6 +4,7 @@
 #include <jsi/jsi.h>
 #include "NativeModules.h"
 #include <JSI/JsiApiContext.h>
+#include <winrt/Windows.Storage.h>
 #include <winrt/Microsoft.ReactNative.h>
 #include <TurboModuleProvider.h>
 #include "bindings.h"
@@ -39,9 +40,15 @@ namespace winrt::OpEngineeringOpSqlite
             auto& runtime = *jsiRuntime;
             auto callInvoker = MakeAbiCallInvoker(m_reactContext.Handle().JSDispatcher());
 
-            
+            // Retrieve the local folder path for the database storage
+            winrt::hstring localFolderPath = winrt::Windows::Storage::ApplicationData::Current().LocalFolder().Path();
+            std::string documentPath = winrt::to_string(localFolderPath);
 
-         //   opsqlite::install(runtime, callInvoker, nullptr, nullptr);
+            // Define the path for the SQLite library if needed, similar to how it's done on iOS.
+            // This example assumes you have a path for the SQLite library
+            std::string crsqlitePath = ""; // Set this to the actual path if needed
+
+            opsqlite::install(runtime, callInvoker, documentPath.c_str(), crsqlitePath.c_str());
             
             return true;
         }
