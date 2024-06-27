d588927addd6989912840f55dd6ba87712c2fa3e
Maksim Windows <maksimlya@gmail.com>
2024-06-27 11:31:43 +0300

    bootstrap windows example.

---
+++ example/windows/op_sqlite_example/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::op_sqlite_example::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::op_sqlite_example::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
